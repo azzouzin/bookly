bad69a0d2b7d824ad7ba6718dbae85da4d103abf
azzouzin <azzouzmerw@gmail.com>
2025-03-23 11:22:53 +0100

    create book details view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/view/widgets/Book%20Details/book_details_view.dart';
 import 'package:go_router/go_router.dart';
 import 'package:bookly/features/splash/presentation/view/splash_view.dart';
 import 'package:bookly/features/home/presentation/view/home_view.dart';
@@ -16,6 +17,14 @@ abstract class AppRouter {
         pageBuilder: (context, state) => CustomTransitionPage(
           key: state.pageKey,
           child: const HomeView(),
+          transitionsBuilder: CustomPageTransitions.slideTransition,
+        ),
+      ),
+      GoRoute(
+        path: '/book_details',
+        pageBuilder: (context, state) => CustomTransitionPage(
+          key: state.pageKey,
+          child: const BookDetailsView(),
           transitionsBuilder: CustomPageTransitions.slideTransition,
         ),
       ),
