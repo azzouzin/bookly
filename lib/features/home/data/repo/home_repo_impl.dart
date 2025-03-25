5da10ecae420cbf5f1901dd3137b32a2fd3772a5
azzouzin <azzouzmerw@gmail.com>
2025-03-25 09:13:53 +0100

    add end point

--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/errors/failliers.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/home/data/model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
@@ -13,15 +14,26 @@ class HomeRepoImpl implements HomeRepo {
   // final HomeRemoteDataSource homeRemoteDataSource;
   // final HomeLocalDataSource homeLocalDataSource;
 
+  ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
   Future<Either<Faillier, List<BookModel>>> fetchBannerBooks() {
     // TODO: implement fetchBannerBooks
+
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Faillier, List<BookModel>>> fetchBestSellerBooks() {
+  Future<Either<Faillier, List<BookModel>>> fetchBestSellerBooks() async {
     // TODO: implement fetchBestSellerBooks
+    try {
+      var data =
+          await apiService.get(endPoint: 'volumes?q=flowers&orderBy=newest');
+      List<BookModel> books = [];
+      books = BookModel.fromList(data.data['items']);
+      return Right(books);
+    } on Exception catch (e) {}
     throw UnimplementedError();
   }
 }
