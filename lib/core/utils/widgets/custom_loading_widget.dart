f7452d1f1cde6608d61f596f2b93943fd51995f4
azzouzin <azzouzmerw@gmail.com>
2025-03-26 11:34:19 +0000

    feat(loading-widget): replace CircularProgressIndicator with Shimmer for better UX
    The Shimmer effect provides a more visually appealing loading experience compared to the default CircularProgressIndicator. This change enhances the user experience during loading states.

--- lib/core/utils/widgets/custom_loading_widget.dart
+++ lib/core/utils/widgets/custom_loading_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomLoadingindicator extends StatelessWidget {
   const CustomLoadingindicator({
@@ -7,6 +8,24 @@ class CustomLoadingindicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: CircularProgressIndicator());
+    return Center(
+      child: Shimmer(
+        gradient: LinearGradient(
+          colors: [
+            Colors.grey.shade300,
+            Colors.grey.shade400,
+            Colors.grey.shade300,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        child: Container(
+          height: 200,
+          width: 150,
+          decoration: BoxDecoration(),
+        ),
+      ),
+    );
+    // return Center(child: CircularProgressIndicator());
   }
 }
